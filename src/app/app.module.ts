6c78894c2b9049fa3b463d0f22be526926e2509e
David East <deast@google.com>
2017-01-05 05:13:58 -0800

    chore(module): Break out AngularFireModule config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
-import { AngularFireModule, AuthMethods, AuthProviders } from 'angularfire2';
+import { ConfiguredAngularFireModule } from './firebase';
 import { SignupModule } from './signup/signup.module';
 
 import { ROUTES } from './app.routes';
@@ -18,16 +18,7 @@ import { NavFooterComponent } from './nav-footer/nav-footer.component';
     BrowserModule,
     ReactiveFormsModule,
     RouterModule.forRoot(ROUTES),
-    AngularFireModule.initializeApp({
-      apiKey: "AIzaSyBwPyYbfFhAe-C4o9yR0_Av7rOSJLKS0mw",
-      authDomain: "af-router-forms.firebaseapp.com",
-      databaseURL: "https://af-router-forms.firebaseio.com",
-      storageBucket: "af-router-forms.appspot.com",
-      messagingSenderId: "163141805580"
-    }, {
-      provider: AuthProviders.Password,
-      method: AuthMethods.Password
-    }),
+    ConfiguredAngularFireModule.configure(),
     SignupModule
   ],
   providers: [],
--- src/app/signup/signup.module.ts
+++ src/app/signup/signup.module.ts
@@ -3,23 +3,14 @@ import { CommonModule } from '@angular/common';
 import { SignupformComponent } from './signupform/signupform.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-import { AngularFireModule, AuthMethods, AuthProviders } from 'angularfire2';
+import { ConfiguredAngularFireModule } from '../firebase';
 import { PopoverModule } from '../popover/popover.module';
 
 @NgModule({
   imports: [
     BrowserModule,
     ReactiveFormsModule,
-    AngularFireModule.initializeApp({
-      apiKey: "AIzaSyBwPyYbfFhAe-C4o9yR0_Av7rOSJLKS0mw",
-      authDomain: "af-router-forms.firebaseapp.com",
-      databaseURL: "https://af-router-forms.firebaseio.com",
-      storageBucket: "af-router-forms.appspot.com",
-      messagingSenderId: "163141805580"
-    }, {
-      provider: AuthProviders.Password,
-      method: AuthMethods.Password
-    }),
+    ConfiguredAngularFireModule.configure(),
     PopoverModule
   ],
   declarations: [
