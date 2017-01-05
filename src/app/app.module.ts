c7e8a573f3ab1fd9d0ee1e69fd592e08743a32da
David East <deast@google.com>
2017-01-05 06:40:54 -0800

    fix(modules): Fix ERROR in Cannot use 'in' operator to search for 'providers' in null

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
-import { ConfiguredAngularFireModule } from './firebase';
 import { SignupModule } from './signup/signup.module';
+import { ConfiguredAngularFireModule } from './firebase';
 
 import { ROUTES } from './app.routes';
 import { AppComponent } from './app.component';
 import { NavFooterComponent } from './nav-footer/nav-footer.component';
+
 
 @NgModule({
   declarations: [
@@ -18,7 +19,7 @@ import { NavFooterComponent } from './nav-footer/nav-footer.component';
     BrowserModule,
     ReactiveFormsModule,
     RouterModule.forRoot(ROUTES),
-    ConfiguredAngularFireModule.configure(),
+    ConfiguredAngularFireModule,
     SignupModule
   ],
   providers: [],
