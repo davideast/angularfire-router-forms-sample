1ac70b8a57e8a00a3a21e221385f09b467cd5011
David East <deast@google.com>
2017-01-05 05:13:58 -0800

    chore(module): Break out AngularFireModule config

---
+++ src/app/firebase/index.ts
@@ -0,0 +1,20 @@
+import { AngularFireModule, AuthMethods, AuthProviders } from 'angularfire2';
+
+export const APP_CONFIG = {
+  apiKey: "AIzaSyBwPyYbfFhAe-C4o9yR0_Av7rOSJLKS0mw",
+  authDomain: "af-router-forms.firebaseapp.com",
+  databaseURL: "https://af-router-forms.firebaseio.com",
+  storageBucket: "af-router-forms.appspot.com",
+  messagingSenderId: "163141805580"
+};
+
+export const AUTH_CONFIG = {
+  provider: AuthProviders.Password,
+  method: AuthMethods.Password
+};
+
+export const ConfiguredAngularFireModule = {
+  configure() {
+    return AngularFireModule.initializeApp(APP_CONFIG, AUTH_CONFIG);
+  }
+};
